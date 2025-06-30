7f687ba8d9667523d5df6f1d38fa9b2eb94f3d57
Sudharshan.S <sudharshan7795@gmail.com>
2025-06-30 22:22:04 +0530

    Update 01.strings.asm

--- 01.strings.asm
+++ 01.strings.asm
@@ -6,87 +6,77 @@ endm
 .model small          
 .stack                
 .data                 
-; Load address of message into DX 
-; DOS function 09h – display string 
-; Interrupt to call DOS function 
-; Define memory model 
-; Define stack segment 
-; Data segment begins 
-; Predefined message strings (with carriage return and line feed) 
+
 m1 db 10,13,"enter string 1:$" 
 m2 db 10,13,"enter string 2:$" 
 m3 db 10,13,"length of string 1 is:$" 
 m4 db 10,13,"length of string 2 is:$" 
 m5 db 10,13,"string1 equal to string2$" 
 m6 db 10,13,"string1 not equal to string2$" 
-; Input buffers for strings (DOS format: MaxLen, ActualLen, Data...) 
+
 str1 db 80 dup(40) 
 str2 db 80 dup(40) 
-; Variables to hold string lengths 
+
 l1 db ? 
 l2 db ? 
 .code               
-; Code segment begins 
-; Initialize data segment registers 
+
 mov ax, @data 
 mov ds, ax 
 mov es, ax 
-; Prompt user for first string 
+
 disp m1 
 lea dx, str1       
 call read         
-; Load address of str1 buffer 
-; Call read procedure to take input 
-; Prompt user for second string 
+
 disp m2 
-    lea dx, str2      ; Load address of str2 buffer 
-    call read         ; Call read procedure to take input 
-    ; Store the actual length of string 1 
+    lea dx, str2      
+    call read
+
     mov al, [str1+1] 
     mov l1, al 
-    ; Store the actual length of string 2 
+    
     mov al, [str2+1] 
     mov l2, al 
-    ; Compare lengths of the two strings 
+     
     cmp al, l1 
-    jne strnote        ; If lengths differ, jump to not equal 
+    jne strnote         
  
-    ; Set up for comparing strings character by character 
+    
     mov ch, 0 
-    mov cl, l1         ; Length of string to compare 
-    lea si, str1+2     ; SI points to first char of str1 
-    lea di, str2+2     ; DI points to first char of str2 
-    cld                 ; Clear direction flag (increment) 
+    mov cl, l1         
+    lea si, str1+2     
+    lea di, str2+2     
+    cld                 
  
-    repe cmpsb         ; Repeat compare while equal 
-    jne strnote        ; If any character mismatched, not equal 
+    repe cmpsb         
+    jne strnote         
  
-    ; If equal 
-    disp m5            ; Display "strings are equal" 
-    jmp next           ; Skip to displaying lengths 
+    
+    disp m5            
+    jmp next           
  
-strnote:              ; If strings are not equal 
-    disp m6           ; Display "strings are not equal" 
+strnote:              
+    disp m6           
 next: 
-    ; Display length of string 1 
+   
     disp m3 
     mov al, l1 
-    call displ         ; Call procedure to display 2-digit number 
-    ; Display length of string 2 
+    call displ        
+    
     disp m4 
     mov al, l2 
-    call displ         ; Call procedure to display 2-digit number 
-    ; Exit program 
+    call displ         
+    
     mov ah, 4ch 
 int 21h 
-; === Procedure to read a string using DOS Function 0Ah === 
+
 read proc 
 mov ah, 0ah        
 int 21h 
 ret 
 read endp 
-; DOS buffered input 
-; === Procedure to display 2-digit decimal number === 
+
 displ proc 
 aam                
 mov bx, ax 
